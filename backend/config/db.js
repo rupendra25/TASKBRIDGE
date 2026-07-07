117b3f979c16470350278211ac3b61577b24f02d
Rupendra Dasari <rupendradasari8@gmail.com>
2026-07-07 14:57:39 +0530

    Done with the changes in the db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,34 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
 
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+
 });
 
-pool.getConnection()
-  .then(connection => {
-    console.log("✅ MySQL Connected");
-    connection.release();
-  })
-  .catch(err => {
-    console.error("❌ MySQL Connection Failed:", err.message);
-  });
+pool.getConnection((err, connection) => {
+
+    if (err) {
+
+        console.error("❌ MySQL Connection Failed:", err.message);
+
+    } else {
+
+        console.log("✅ MySQL Connected");
+
+        connection.release();
+
+    }
+
+});
 
 module.exports = pool;
